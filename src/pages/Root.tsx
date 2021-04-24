d871f3bc6073fb2843b6edaba78366f3a7dffce7
lrbengozi <lrbengozi@gmail.com>
2021-04-24 10:38:05 -0300

    Add Router

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import HomePage from './Home/Index'
 import LoginPage from './Login/index'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 import '../styles/global.scss'
 
 
 const PagesRoot = () => {
-  return(
-    <HomePage />  
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
+    </Router>
   )
 }
 
