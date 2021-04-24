986414a715b10fcf2d6ab03083d3d2c7fda315a0
lrbengozi <lrbengozi@gmail.com>
2021-04-24 17:18:36 -0300

    Crete login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -6,9 +6,25 @@ const LoginPage = () => {
   return (
     <div className={styles.loginContainer}>
       <div className={styles.loginBox}>
-        <form autoComplete="nope" className={styles.loginForm}>
-          
-        </form>
+        <div className={styles.loginForm}>
+          <input type="email" />
+          <label htmlFor="email">
+            E-mail
+          </label>
+        </div>
+        <div className={styles.loginForm}>
+          <input type="password" />
+          <label htmlFor="password">
+            Senha
+          </label>
+        </div>
+        <div className={styles.buttons}>
+          <button type="button" value='Login'>
+            <p>
+              Login
+          </p>
+          </button>
+        </div>
       </div>
     </div>
   )
