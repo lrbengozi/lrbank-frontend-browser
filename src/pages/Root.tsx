a2ae29a52a29eb265d222311b3c60495b7429f4a
lrbengozi <lrbengozi@gmail.com>
2021-04-24 10:18:00 -0300

    Creates page structure

---
+++ src/pages/Root.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePage from './Home/Index'
+import LoginPage from './Login/index'
+
+
+const PagesRoot = () => {
+  return(
+    <HomePage />  
+  )
+}
+
+export default PagesRoot;
